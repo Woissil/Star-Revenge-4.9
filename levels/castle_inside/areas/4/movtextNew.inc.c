7a917b05a5d7c23f92a4890bf72373c4c6ba076e
Woissil <114301097+Woissil@users.noreply.github.com>
2023-07-20 19:31:25 +0200

    Delete levels/castle_inside directory

--- levels/castle_inside/areas/4/movtextNew.inc.c
+++
@@ -1,9 +0,0 @@
-const struct MovtexQuadCollection castle_inside_4_Movtex_0[] = {
-{-1, NULL},
-};
-const struct MovtexQuadCollection castle_inside_4_Movtex_1[] = {
-{-1, NULL},
-};
-const struct MovtexQuadCollection castle_inside_4_Movtex_2[] = {
-{-1, NULL},
-};
