a89d8bb2eb17b8fa8d73f0fd2f05d4ecd3f4017d
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-03-31 21:28:00 +0330

    fix act selector stars

--- star.lua
+++ star.lua
@@ -15,30 +15,37 @@ E_MODEL_TTCSTAR = smlua_model_util_get_id("ttcstar_geo")
 E_MODEL_RRSTAR = smlua_model_util_get_id("rrstar_geo")
 
 starStateTable = {
-    [COURSE_BOB] = E_MODEL_STAR,
-    [COURSE_WF] = E_MODEL_WFSTAR,
-    [COURSE_JRB] = E_MODEL_JRBSTAR,
-    [COURSE_CCM] = E_MODEL_CCMSTAR,
-    [COURSE_BBH] = E_MODEL_BBHSTAR,
-    [COURSE_HMC] = E_MODEL_HMCSTAR,
-    [COURSE_LLL] = E_MODEL_LLLSTAR,
-    [COURSE_SSL] = E_MODEL_SSLSTAR,
-    [COURSE_DDD] = E_MODEL_DDDSTAR,
-    [COURSE_SL] = E_MODEL_SLSTAR,
-    [COURSE_WDW] = E_MODEL_WDWSTAR,
-    [COURSE_TTM] = E_MODEL_TTMSTAR,
-    [COURSE_THI] = E_MODEL_THISTAR,
-    [COURSE_TTC] = E_MODEL_TTCSTAR,
-    [COURSE_RR] = E_MODEL_RRSTAR,
+    [LEVEL_BOB] = E_MODEL_STAR,
+    [LEVEL_WF] = E_MODEL_WFSTAR,
+    [LEVEL_JRB] = E_MODEL_JRBSTAR,
+    [LEVEL_CCM] = E_MODEL_CCMSTAR,
+    [LEVEL_BBH] = E_MODEL_BBHSTAR,
+    [LEVEL_HMC] = E_MODEL_HMCSTAR,
+    [LEVEL_LLL] = E_MODEL_LLLSTAR,
+    [LEVEL_SSL] = E_MODEL_SSLSTAR,
+    [LEVEL_DDD] = E_MODEL_DDDSTAR,
+    [LEVEL_SL] = E_MODEL_SLSTAR,
+    [LEVEL_WDW] = E_MODEL_WDWSTAR,
+    [LEVEL_TTM] = E_MODEL_TTMSTAR,
+    [LEVEL_THI] = E_MODEL_THISTAR,
+    [LEVEL_TTC] = E_MODEL_TTCSTAR,
+    [LEVEL_RR] = E_MODEL_RRSTAR,
 }
-
+---@type NetworkPlayer
+local np = gNetworkPlayers[0]
 function init_star_model(o)
     if obj_has_model_extended(o, E_MODEL_TRANSPARENT_STAR) == 0 then
-        obj_set_model_extended(o, starStateTable[gNetworkPlayers[0].currCourseNum])
+        if obj_has_behavior_id(o, id_bhvActSelectorStarType) == 0 then
+            obj_set_model_extended(o, starStateTable[np.currLevelNum])
+        end
+
+        if obj_has_behavior_id(o, id_bhvActSelectorStarType) ~= 0 and starStateTable[np.currLevelNum] ~= nil then
+            obj_set_model_extended(o, starStateTable[np.currLevelNum])
+        end
     end
 end
 
-hook_behavior(id_bhvActSelectorStarType, OBJ_LIST_DEFAULT, false, init_star_model, nil)
+hook_behavior(id_bhvActSelectorStarType, OBJ_LIST_DEFAULT, false, nil, init_star_model)
 hook_behavior(id_bhvStar, OBJ_LIST_LEVEL, false, init_star_model, nil)
 hook_behavior(id_bhvCelebrationStar, OBJ_LIST_GENACTOR, false, nil, init_star_model)
 hook_behavior(id_bhvStarSpawnCoordinates, OBJ_LIST_GENACTOR, false, nil, init_star_model)
